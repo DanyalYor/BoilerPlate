0cf26027669a39182f034a8886d09a339684111c
dayo <dayo@itu.dk>
2021-09-08 19:33:40 +0200

    Boilerplate project done

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoilerPlate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
