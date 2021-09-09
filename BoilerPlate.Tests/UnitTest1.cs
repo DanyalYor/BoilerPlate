191b6d1f5582ba4166a3a649742b495c599fe38f
dayo <dayo@itu.dk>
2021-09-09 10:45:41 +0200

    exercise 5 & 6 finished

--- BoilerPlate.Tests/UnitTest1.cs
+++ BoilerPlate.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace BoilerPlate.Tests
@@ -6,9 +7,25 @@ namespace BoilerPlate.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void IsLeapYear_Checks_Leap_Year()
         {
+            //Arrange
+            var method = new Program();
+            var fail = 1900;
+            var success1 = 2000;
+            var success2 = 8;
 
+            //Act
+            var giveFalse = method.IsLeapYear(fail);
+            var giveTrue1 = method.IsLeapYear(success1);
+            var giveTrue2 = method.IsLeapYear(success2);
+
+            //Assert
+            Assert.False(giveFalse);
+            Assert.True(giveTrue1);
+            Assert.True(giveTrue2);
+
+            Console.WriteLine("jdwnajdnajfnekajnncjkadncjkndjkacndjknakjncdjan");
         }
     }
 }
--- BoilerPlate/Program.cs
+++ BoilerPlate/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
 
 namespace BoilerPlate
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            Console.WriteLine("Please type in a year!");
+            string input = Console.ReadLine();
+            int inputInt = Int32.Parse(input);
+
+            Console.WriteLine(program.IsLeapYear(inputInt) ? "yay" : "nay");
+        }
+
+        public bool IsLeapYear(int year)
+        {
+            if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+            {
+                return true;
+            } 
+            return false;
         }
     }
 }
